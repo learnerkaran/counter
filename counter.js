a0af390a4e03f48ce15e37cdbc839764e7a6db74
learnerkaran <168950203+learnerkaran@users.noreply.github.com>
2024-08-03 22:13:41 +0530

    counter.js

--- counter.js
+++ counter.js
@@ -1,43 +1,33 @@
-let counterElement = document.getElementById('counterValue')
+let counterElement = document.getElementById('counterValue');
 
 function onIncrement() {
-    let previousCounterValue = counterElement.textContent;
-    let updatedCountervalue = parseInt(previousCounterValue) + 1;
-    counterElement.textContent = updatedCountervalue;
+    let previousCounterValue = parseInt(counterElement.textContent);
+    let updatedCounterValue = previousCounterValue + 1;
+    counterElement.textContent = updatedCounterValue;
     
-   if (updatedCountervalue > 0) {
-       counterElement.style.color = 'green'
-   }
-   else if (updatedCountervalue < 0) {
-       counterElement.style.color = 'red'
-   }
-   else {
-       counterElement.style.color = 'black'
-   }
-
+    updateCounterColor(updatedCounterValue);
 }
 
 function onDecrement() {
-    let previousCounterValue = counterElement.textContent;
-    let updatedCountervalue = parseInt(previousCounterValue) - 1;
-    counterElement.textContent = updatedCountervalue;
-    console.log(updatedCountervalue);
-     if (updatedCountervalue > 0) {
-       counterElement.style.color = 'green'
-   }
-   else if (updatedCountervalue < 0) {
-       counterElement.style.color = 'red'
-   }
-   else {
-       counterElement.style.color = 'black'
-   }
+    let previousCounterValue = parseInt(counterElement.textContent);
+    let updatedCounterValue = previousCounterValue - 1;
+    counterElement.textContent = updatedCounterValue;
 
-
+    updateCounterColor(updatedCounterValue);
 }
 
 function onReset() {
-    let updatedCountervalue = 0;
-    counterElement.textContent = updatedCountervalue;
-    counterElement.style.color = 'black'
+    let updatedCounterValue = 0;
+    counterElement.textContent = updatedCounterValue;
+    counterElement.style.color = 'black';
+}
 
+function updateCounterColor(value) {
+    if (value > 0) {
+        counterElement.style.color = 'green';
+    } else if (value < 0) {
+        counterElement.style.color = 'red';
+    } else {
+        counterElement.style.color = 'black';
+    }
 }
